d8626a8df4e94fd904d37d62cd01d1a4c27b91e6
lsobik <hukash@gmail.com>
2010-10-06 21:52:07 +0200

    deleted node-couchdb, removed couch code

--- server.js
+++ server.js
@@ -7,20 +7,3 @@ process.addListener('uncaught Exception', function(err, stack){
   console.log('---------------');
 });
 
-// var CouchClient = require('.lib/couchclient');
-
-var sys = require('sys'),
-    couchdb = require('node-couchdb/lib/couchdb'),
-    client = couchdb.createClient(5984, 'localhost'),
-    db = client.db('my-db');
-
-db.saveDoc('my-doc', {awesome: 'couch fun'}, function(err, ok){
-  if (err) throw new Error(JSON.stringify(err));
-  sys.puts('Saved my first doc to the couch');
-});
-
-db.getDoc('my-doc', function(err, doc){
-  if (err) throw new Error(JSON.stringify(err));
-  sys.puts('Fetch my doc from couch:');
-  sys.puts(sys.inspect(doc));
-});
